34b765a21007f55d7679c4b627786b7bef8845ba
BalaurBohemianBroken <realvulpes@gmail.com>
2026-06-18 13:36:04 +0100

    Change how RNG is compared.

--- OwOText/OwOText.cs
+++ OwOText/OwOText.cs
@@ -8,16 +8,17 @@ using System.Reflection;
 using BepInEx.Logging;
 
 namespace OwOText {
-    [BepInPlugin("com.balaur.OwO", "OwOText", "0.1.1")]
+    [BepInPlugin("com.balaur.OwO", "OwOText", "0.1.2")]
     public class OwOText : BaseUnityPlugin {
         public static int seed = new Random().Next(1000);
         
-        public static int stutter_die = 20;
-        public static int sub_fullstop_die = 5;
-        public static int punct_repeat_die = 3;
-        public static int chaos_die = 10;
-        public static int tilde_die = 10;
-        public static int face_die = 8;
+        public static double stutter_chance = 1.0/20;
+        public static double sub_fullstop_chance = 1.0/5;
+        public static double punct_repeat_chance = 1.0/3;
+        public static int punct_repeat_limit = 5;
+        public static double chaos_chance = 1.0/10;
+        public static double tilde_chance = 1.0/10;
+        public static double face_chance = 1.0/8;
         public static string[] faces =
             { ":<", ":>", "c:", ":c", "umu", ">//>", "x3", ">v>", "UvU", ":3", ":3c", ":P", ":p", "-w-", "=w=", ";w;", ">w>", "<w<", "<//<", "<v<" };
 
@@ -35,31 +36,10 @@ namespace OwOText {
             DoPatch(harmony, target, prefix: patch);
         }
 
-        // private void DoPrefix(Harmony harmony, Type targetClass, string targetMethod, Type patchClass, string patchMethod) {
-        //     Logger.LogInfo("Patching...");
-        //     
-        //     Logger.LogInfo("Getting patch...");
-        //     var patch = patchClass.GetMethod(patchMethod);
-        //     if (patch == null) {
-        //         Logger.LogError($"Could not find method {patchMethod} on class {patchClass}");
-        //         return;
-        //     }
-        //     Logger.LogInfo("Getting target...");
-        //     var target = targetClass.GetMethod(targetMethod);
-        //     if (target == null) {
-        //         Logger.LogError($"Could not find method {targetMethod} on class {targetClass}");
-        //         return;
-        //     }
-        //     
-        //     Logger.LogInfo("Doing patch...");
-        //     harmony.Patch(target, prefix: new HarmonyMethod(patch));
-        //     Logger.LogInfo($"Patched {targetMethod} with {patchMethod}.");
-        // }
-
-        private void DoPatch(Harmony harmony, MethodInfo target, MethodInfo? prefix = null) {
-            var pre = new HarmonyMethod(prefix);
-            // var post = postfix != null ? new HarmonyMethod(postfix) : null;
-            harmony.Patch(target, prefix: pre);
+        private void DoPatch(Harmony harmony, MethodInfo target, MethodInfo? prefix = null, MethodInfo? postfix = null) {
+            var pre = prefix != null ? new HarmonyMethod(prefix) : null;
+            var post = postfix != null ? new HarmonyMethod(postfix) : null;
+            harmony.Patch(target, prefix: pre, postfix: post);
         }
 
         public static bool PatchTalker(List<string> lines) {
@@ -91,13 +71,13 @@ namespace OwOText {
                 this_loop += Chaos(c, rng);
                 c = this_loop != "" ? this_loop[^1] : c;
                 
-                this_loop += Tilde(c, rng);;
+                this_loop += Tilde(c, rng);
                 c = this_loop != "" ? this_loop[^1] : c;
                 
-                this_loop += Excitement(c, rng);;
+                this_loop += Excitement(c, rng);
                 c = this_loop != "" ? this_loop[^1] : c;
                 
-                this_loop += Faces(input, i, c, rng);;
+                this_loop += Faces(input, i, c, rng);
                 c = this_loop != "" ? this_loop[^1] : c;
 
                 if (this_loop == "")
@@ -133,7 +113,7 @@ namespace OwOText {
 
             string new_string = "";
             bool first = true;
-            while (rng.Next(stutter_die) == 0) {
+            while (rng.NextDouble() < stutter_chance) {
                 if (first) {
                     new_string = character.ToString();
                     first = false;
@@ -172,7 +152,7 @@ namespace OwOText {
         private static string Chaos(char character, Random rng) {
             if ("ouOU".IndexOf(character) == -1)
                 return "";
-            if (rng.Next(chaos_die) != 0)
+            if (!(chaos_chance > rng.NextDouble()))
                 return "";
 
             switch (character) {
@@ -190,7 +170,7 @@ namespace OwOText {
         }
 
         private static string Tilde(char character, Random rng) {
-            if (".!?".IndexOf(character) == -1 || rng.Next(tilde_die) != 0) {
+            if (".!?".IndexOf(character) == -1 || !(tilde_chance > rng.NextDouble())) {
                 return "";
             }
             
@@ -210,24 +190,26 @@ namespace OwOText {
 
             string new_string = "";
             if (character == '.') {
-                if (rng.Next(sub_fullstop_die) == 0) {
+                if (sub_fullstop_chance > rng.NextDouble()) {
                     character = '!';
                     new_string = "!";
                 }
             }
 
-            while (rng.Next(punct_repeat_die) == 0) {
+            int repeats = 0;
+            while (punct_repeat_chance > rng.NextDouble() && repeats < punct_repeat_limit) {
                 if (new_string == "") {
                     new_string = character.ToString();
                 }
                 new_string += character.ToString();
+                repeats += 1;
             }
 
             return new_string;
         }
 
         private static string Faces(string source, int index, char character, Random rng) {
-            if (rng.Next(face_die) != 0) {
+            if (!(face_chance > rng.NextDouble())) {
                 return "";
             }
             string face = faces[rng.Next(faces.Length)];
